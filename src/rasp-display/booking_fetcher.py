3b0b4f92c87fdfcf7641348f156d1065f2e217eb
lucadibello <luca.dibello@samtrevano.ch>
2019-12-11 23:03:57 +0100

    Python update

--- src/rasp-display/booking_fetcher.py
+++ src/rasp-display/booking_fetcher.py
@@ -7,16 +7,43 @@ import logging
 
 # Create and setup logger
 LOGGER_FORMAT = "[%(name)s] %(levelname)s -  %(asctime)s: %(message)s"
-logging.basicConfig(format=LOGGER_FORMAT, filename='fetcher.log')
+logging.basicConfig(format=LOGGER_FORMAT, filename='fetcher.log', level=logging.DEBUG)
 logger = logging.getLogger("FETCHER")
 
 # Module usefull for scheduled operations
 import sched
 import time
 
+# Os lib for directories management
+import os
+
+# Lib for http requests
+import requests
+
 
 def fetcher(config: dict):
-    print("Yolino")
+    logger.info("Starting update process...")
+
+    # Fix data path is necessary
+    _fix_filepath(config.get("booking_data_path"))
+
+    # Update local data
+    with open(config.get("booking_data_path"), 'w+') as f:
+        # Read booking data from APIs
+        data = requests.post(config.get("api_url"), data={"token": config.get("api_token")}).text
+        logger.debug("Read data: %s bytes", len(data))
+        f.write(data)
+        logger.info("Booking data saved locally")
+
+
+def _fix_filepath(filepath):
+    if not os.path.exists(os.path.dirname(filepath)):
+        logger.warning("Data filepath not found")
+        try:
+            os.makedirs(os.path.dirname(filepath))
+            logger.info("Data filepath fixed")
+        except OSError as exc:  # Guard against race condition
+            logger.warning(exc)
 
 if __name__ == '__main__':
     # Load config from config file
